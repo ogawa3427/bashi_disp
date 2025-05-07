4514229742e026970f0f7633cf6b51727e7b4651
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2025-05-08 04:16:48 +0900

    色

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'dart:js' as js;
 import 'dart:html' as html;
 import 'dart:ui' as ui;
+import 'package:marquee/marquee.dart';
 
 // 1920x540
 
@@ -36,7 +37,7 @@ class _ResolutionDisplayPageState extends State<ResolutionDisplayPage> {
   bool _isFullScreen = false;
   bool _buttonVisible = true; // ボタンの表示状態を管理
   // 横分割の比率を設定するリスト
-  final List<int> _columnRatios = [5, 3, 7, 18]; // 時刻、路線番号、行先、経由の比率
+  final List<int> _columnRatios = [5, 3, 9, 16]; // 時刻、路線番号、行先、経由の比率
 
   // セルテキスト用の共通スタイルを定義
   final TextStyle cellTextStyle = const TextStyle(
@@ -44,12 +45,9 @@ class _ResolutionDisplayPageState extends State<ResolutionDisplayPage> {
     fontSize: 105,
     fontWeight: FontWeight.normal,
     shadows: [
-      Shadow(
-        blurRadius: 2.0,
-        color: Colors.black,
-        offset: Offset(1.0, 1.0),
-      ),
+      Shadow(blurRadius: 2.0, color: Colors.black, offset: Offset(1.0, 1.0)),
     ],
+    height: 1,
   );
 
   @override
@@ -103,9 +101,278 @@ class _ResolutionDisplayPageState extends State<ResolutionDisplayPage> {
 
     return Scaffold(
       body: Container(
-        color: Colors.blue,
+        color: Colors.black,
         child: Stack(
           children: [
+            Container(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: _columnRatios[0],
+                          child: Container(
+                            child: Center(
+                              child: Padding(
+                                // padding: EdgeInsets.only(left: 60),
+                                padding: EdgeInsets.only(left: 10),
+                                child: Text(
+                                  '11:56',
+                                  style: cellTextStyle,
+                                  textAlign: TextAlign.right,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: _columnRatios[1],
+                          child: Container(
+                            padding: EdgeInsets.all(5),
+                            // color: Colors.green.shade900,
+                            child: Center(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(
+                                    color: Colors.yellow,
+                                    width: 10,
+                                  ),
+                                ),
+                                margin: EdgeInsets.zero,
+                                // padding: EdgeInsets.all(5),
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: Center(
+                                  child: Stack(
+                                    children: [
+                                      Container(
+                                        // color: Colors.red,
+                                        child: Center(
+                                          child: Text(
+                                            '94',
+                                            // style: cellTextStyle,
+                                            style: cellTextStyle.copyWith(
+                                              color: Colors.black,
+                                              // fontWeight: FontWeight.bold,
+                                              // 縁取り
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: _columnRatios[2],
+                          child: Container(
+                            // color: Colors.blue.shade900,
+                            child: Center(
+                              child: Text('金沢駅', style: cellTextStyle),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: _columnRatios[3],
+                          child: Container(
+                            // color: Colors.yellow.shade900,
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 10),
+                                child: Text(
+                                  "旭町経由",
+                                  style: cellTextStyle.copyWith(
+                                    color: Colors.lightBlueAccent,
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: _columnRatios[0],
+                          child: Container(
+                            // color: Colors.yellow.shade700,
+                            child: Center(
+                              child: Text(
+                                "若松からの停車停留所:若松西 若松橋 旭町 旭町口 田井町 桜町から 兼六園下 香林坊 武蔵ヶ辻 経由 金沢駅東口まで",
+                                style: cellTextStyle,
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: _columnRatios[0],
+                          child: Container(
+                            child: Center(
+                              child: Padding(
+                                padding: EdgeInsets.only(right: 0),
+                                child: Text(
+                                  '12:13',
+                                  style: cellTextStyle,
+                                  textAlign: TextAlign.right,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: _columnRatios[1],
+                          child: Container(
+                            // color: Colors.green.shade500,
+                            child: Center(
+                              child: Text('', style: cellTextStyle),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: _columnRatios[2],
+                          child: Container(
+                            // color: Colors.blue.shade500,
+                            child: Center(
+                              child: Text('田井町', style: cellTextStyle),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: _columnRatios[3],
+                          child: Container(
+                            // color: Colors.yellow.shade500,
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 10),
+                                child: Text(
+                                  '旭町経由',
+                                  style: cellTextStyle.copyWith(
+                                    color: Colors.lightBlueAccent,
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: _columnRatios[0],
+                          child: Container(
+                            child: Center(
+                              child: Padding(
+                                padding: EdgeInsets.only(right: 0),
+                                child: Text(
+                                  '12:16',
+                                  style: cellTextStyle,
+                                  textAlign: TextAlign.right,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: _columnRatios[1],
+                          child: Container(
+                            padding: EdgeInsets.all(5),
+                            // color: Colors.green.shade900,
+                            child: Center(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  // color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(
+                                    color: Colors.yellow,
+                                    width: 10,
+                                  ),
+                                ),
+                                margin: EdgeInsets.zero,
+                                // padding: EdgeInsets.all(5),
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: Center(
+                                  child: Stack(
+                                    children: [
+                                      Container(
+                                        // color: Colors.red,
+                                        child: Center(
+                                          child: Text(
+                                            '93',
+                                            // style: cellTextStyle,
+                                            style: cellTextStyle.copyWith(
+                                              color: Colors.yellow,
+                                              // fontWeight: FontWeight.bold,
+                                              // 縁取り
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: _columnRatios[2],
+                          child: Container(
+                            // color: Colors.blue.shade300,
+                            child: Center(
+                              child: Text('金沢駅', style: cellTextStyle),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: _columnRatios[3],
+                          child: Container(
+                            // color: Colors.yellow.shade300,
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 10),
+                                child: Text(
+                                  '鈴見経由',
+                                  style: cellTextStyle.copyWith(
+                                    color: Colors.lightGreenAccent,
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ), // 二層目のレ
             Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -125,113 +392,6 @@ class _ResolutionDisplayPageState extends State<ResolutionDisplayPage> {
                 ],
               ),
             ),
-            Container(
-              // 横三分割から縦三分割に変更
-              child: Column(
-                children: [
-                  Expanded(
-                    child: Row(
-                      children: [
-                        Expanded(flex: _columnRatios[0], child: Container(
-                          color: Colors.red.shade900,
-                          child: Align(
-                            alignment: Alignment.centerRight,
-                            child: Padding(
-                              padding: EdgeInsets.only(right: 20),
-                              child: Text('9:05', style: cellTextStyle),
-                            ),
-                          ),
-                        )),
-                        Expanded(flex: _columnRatios[1], child: Container(
-                          color: Colors.green.shade900,
-                          child: Center(child: Text('94', style: cellTextStyle)),
-                        )),
-                        Expanded(flex: _columnRatios[2], child: Container(
-                          color: Colors.blue.shade900,
-                          child: Center(child: Text('金沢駅', style: cellTextStyle)),
-                        )),
-                        Expanded(flex: _columnRatios[3], child: Container(
-                          color: Colors.yellow.shade900,
-                          child: Center(child: Text("<旭町経由>", style: cellTextStyle)),
-                        )),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Row(
-                      children: [
-                        Expanded(flex: _columnRatios[0], child: Container(
-                          color: Colors.yellow.shade700,
-                          child: Center(
-                            child: Text(
-                              "若松からの停車停留所:若松西 若松橋 旭町 旭町口 田井町 桜町から 兼六園下 香林坊 武蔵ヶ辻 経由 金沢駅東口まで", 
-                              style: cellTextStyle,
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        )),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Row(
-                      children: [
-                        Expanded(flex: _columnRatios[0], child: Container(
-                          color: Colors.red.shade500,
-                          child: Align(
-                            alignment: Alignment.centerRight,
-                            child: Padding(
-                              padding: EdgeInsets.only(right: 20),
-                              child: Text('11:00', style: cellTextStyle),
-                            ),
-                          ),
-                        )),
-                        Expanded(flex: _columnRatios[1], child: Container(
-                          color: Colors.green.shade500,
-                          child: Center(child: Text('', style: cellTextStyle)),
-                        )),
-                        Expanded(flex: _columnRatios[2], child: Container(
-                          color: Colors.blue.shade500,
-                          child: Center(child: Text('田井町', style: cellTextStyle)),
-                        )),
-                        Expanded(flex: _columnRatios[3], child: Container(
-                          color: Colors.yellow.shade500,
-                          child: Center(child: Text('<旭町経由>', style: cellTextStyle)),
-                        )),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Row(
-                      children: [
-                        Expanded(flex: _columnRatios[0], child: Container(
-                          color: Colors.red.shade300,
-                          child: Align(
-                            alignment: Alignment.centerRight,
-                            child: Padding(
-                              padding: EdgeInsets.only(right: 20),
-                              child: Text('18:32', style: cellTextStyle),
-                            ),
-                          ),
-                        )),
-                        Expanded(flex: _columnRatios[1], child: Container(
-                          color: Colors.green.shade300,
-                          child: Center(child: Text('93', style: cellTextStyle)),
-                        )),
-                        Expanded(flex: _columnRatios[2], child: Container(
-                          color: Colors.blue.shade300,
-                          child: Center(child: Text('金沢駅', style: cellTextStyle)),
-                        )),
-                        Expanded(flex: _columnRatios[3], child: Container(
-                          color: Colors.yellow.shade300,
-                          child: Center(child: Text('[鈴見経由]', style: cellTextStyle)),
-                        )),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ), // 二層目のレイヤー（必要に応じて内容を追加可能）
           ],
         ),
       ),
